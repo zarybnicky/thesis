668c83bfe91ffbeb0cc589a516a5606528bf11ca
Jakub Zárybnický <kubaz@inuits.eu>
2019-04-17 15:05:19 +0200

    routing-record: Implemented in-app links

--- src-snippets/routing-record/Main.hs
+++ src-snippets/routing-record/Main.hs
@@ -1,74 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main
   ( main
   ) where
 
+import Data.Proxy (Proxy(..))
+import qualified Data.Text as T
 import Language.Javascript.JSaddle.Warp as JS (run)
+import GHC.Generics (Generic)
 import Reflex.Dom.Core hiding (Link, Widget)
-import Servant.App (app)
+import Servant.App
+import Servant.API
 
 main :: IO ()
 main = run 3000 $ mainWidget app
 
+app :: MonadWidget t m => m ()
+app = serve (Proxy @(ToServantApi Routes)) (toServant widgets) (text . T.pack . show)
 
+widgets :: forall t m. MonadWidget t m => Routes (AsApp t (EventWriterT t Loc m))
+widgets = Routes
+  { showUserRoute = \(i :: Int) -> text (T.pack $ show i)
+  , homeRoute = do
+      e <- button "Go to user"
+      appLink showUserRoute (5 <$ e)
+  }
 
--- app :: forall t m. MonadWidget t m => m ()
--- app = void $ (void $ serve api localhost widgets errorPageLoc errorPage)
---   where localhost = pure (Authority Http "localhost" (Just 8080) "")
-
--- data Item = Item
---   { itemId :: Int
---   , itemName :: String
---   , itemPrice :: Double
---   } deriving (Show, Generic, FromJSON, ToJSON)
-
--- type API = "item" :> "all" :> Get '[JSON,RunTime] [Item]
---       :<|> "item" :> "one" :> Capture "itemId" Int :> Get '[JSON,RunTime] Item
---       :<|> "item" :> "num" :> Reflexive (Get '[JSON] Int)
---       :<|> Get '[JSON,RunTime] ()
-
--- api :: Proxy API
--- api = Proxy
-
--- item :: MonadWidget t m => Item -> m ()
--- item (Item i name p) =
---   el "div" $ do
---     el "div" $ text $ "Item: "  <> T.pack name
---     el "div" $ text $ "Id: "    <> T.pack (show i)
---     el "div" $ text $ "Price: " <> T.pack (show p)
-
--- widgets :: MonadWidget t m => Links API t m -> Widgets API t m
--- widgets (jumpAll :<|> jumpOne :<|> eLens :<|> jumpHome) =
---   displayAll jumpOne jumpHome :<|> displayOne jumpAll jumpHome :<|> () :<|> displayHome eLens jumpAll
---   where displayAll jumpOne jumpHome items = Link $ do
---           mapM_ (el "div" . item) items
---           el "div" $ text "Jump to an item based off of its id: "
---           t <- textInput $ def & textInputConfig_inputType .~ "number"
---                                & textInputConfig_initialValue .~ "0"
---           unLink $ jumpOne (fmap (maybe 0 id . readMaybe . T.unpack) (_textInput_value t))
---                            (fmapMaybe (\x -> if keyCodeLookup x == Enter then Just () else Nothing)
---                                       (_textInput_keypress t))
---         displayOne jumpAll jumpHome i = Link $ do
---           item i
---           h <- button "Jump home!"
---           unLink (jumpHome h)
---         displayHome eLens jumpAll () = Link $ do
---           n <- button "Refresh length"
---           len <- fmapMaybe hush <$> eLens n
---           el "div" $ do
---             text "Number of items:"
---             holdDyn 0 len >>= display
---           a <- button "Jump All!"
---           unLink (jumpAll a)
---         hush (Left _)  = Nothing
---         hush (Right a) = Just a
-
--- errorPage :: (Monad m, DomBuilder t m) => Links API t m -> ServantErr -> Link t m
--- errorPage (_ :<|> _ :<|> _ :<|> jumpHome) err = Link $ do
---   el "div" $ text $ "Something went wrong : " <> displayErr err
---   b <- button "Jump home!"
---   unLink (jumpHome b)
---   where displayErr (NotFound err) = "Not Found: " <> err
---         displayErr (AjaxFailure err) = "Ajax failure: " <> err
-
--- errorPageLoc :: Uri
--- errorPageLoc = Uri ["error"] []
+data Routes route = Routes
+  { showUserRoute :: route :- "user" :> Capture "id" Int :> App
+  , homeRoute :: route :- App
+  } deriving (Generic)
--- src-snippets/routing-record/Servant/App.hs
+++ src-snippets/routing-record/Servant/App.hs
@@ -1,9 +1,7 @@
-
-
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds #-}
@@ -13,13 +11,26 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Servant.App
-  ( app
+  ( Loc(..)
+  , Err(..)
+  , App
+  , AsApp
+  , HasApp(..)
+  , HasAppLink(..)
+  , serve
+  , url
+  , appLink
+  , TupleProduct(..)
+  , GenericMode(..)
+  , ToServantApi
+  , toServant
   ) where
 
-import Data.Aeson (FromJSON, eitherDecode)
-import Data.Bifunctor (bimap)
+import Data.Aeson (FromJSON, ToJSON, eitherDecode, encode)
+import Data.Bifunctor (bimap, first)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as BC
 import Data.Either (partitionEithers)
@@ -35,34 +46,53 @@ import GHCJS.DOM.Location (getHref)
 import GHCJS.DOM.Types (MonadJSM)
 import GHCJS.DOM.Window (getHistory, getLocation)
 import GHCJS.DOM.WindowEventHandlers (popState)
-import Network.HTTP.Media ((//), (/:))
 import Reflex.Dom.Core
-import Servant.API hiding (URI(..))
-import Servant.API.Generic
-import Servant.API.TypeLevel
+import Servant.API ((:<|>)(..), (:>), Capture, IsElem, QueryParam, QueryParams)
+import Servant.API.Generic (AsApi, GenericMode(..), GenericServant, ToServantApi, genericApi, toServant)
 import URI.ByteString
+  (Query(..), URI, URIParseError, laxURIParserOptions, parseURI,
+   serializeURIRef', uriFragment, uriPath, uriQuery)
 
+
+data Loc = Loc
+  { locPath :: [ByteString]
+  , locQuery :: [(ByteString, ByteString)]
+  }
+instance Semigroup Loc where
+  a <> _ = a
+
+data Err
+  = Err400 Text
+  | Err401
+  | Err404
+  | Err500 URIParseError
+  | Err501
+  deriving (Eq, Show)
 
 url ::
      ( MonadHold t m
      , TriggerEvent t m
      , PerformEvent t m
+     , DomBuilder t m
      , MonadJSM (Performable m)
      , MonadJSM m
      )
   => Event t Loc
-  -> m (Dynamic t (Either URIParseError Loc))
+  -> m (Dynamic t (Either Err Loc))
 url us = do
   window <- currentWindowUnchecked
   location <- getLocation window
   history <- getHistory window
-  loc0 <- text2uri <$> getHref location
-  let u0 = either (error . ("Failed to parse URI: " <>) . show) id loc0
-  performEvent_ $ pushState history () ("" :: Text) . Just . uri2text . loc2uri u0 <$> us
-  ps <- wrapDomEvent window (`on` popState) $ fmap uri2loc . text2uri <$> getHref location
-  holdDyn (Right $ uri2loc u0) (leftmost [ps, Right <$> us])
+  text2uri <$> getHref location >>= \case
+    Left e -> do
+      text ("Failed to parse URI, cannot continue: " <> T.pack (show e))
+      pure . constDyn $ Left e
+    Right u0 -> do
+      performEvent_ $ pushState history () ("" :: Text) . Just . uri2text . loc2uri u0 <$> us
+      ps <- wrapDomEvent window (`on` popState) $ fmap uri2loc . text2uri <$> getHref location
+      holdDyn (Right $ uri2loc u0) (leftmost [ps, Right <$> us])
   where
-    text2uri = parseURI laxURIParserOptions . BC.toStrict . BC.pack . T.unpack
+    text2uri = first Err500 . parseURI laxURIParserOptions . BC.toStrict . BC.pack . T.unpack
     uri2text = T.pack . BC.unpack . BC.fromStrict . serializeURIRef'
     uri2loc uri = Loc
       { locPath = drop 1 . BC.split '/' . BC.fromStrict $ uriPath uri
@@ -74,6 +104,24 @@ url us = do
       , uriQuery = Query $ bimap BC.toStrict BC.toStrict <$> locQuery loc
       , uriFragment = Nothing
       }
+
+
+-- error page :: (api -> AppT) -> Err -> m ()
+-- Think about /error?redirect=/admin/x
+
+serve ::
+     forall t m api. (HasApp api (), MonadWidget t m)
+  => Proxy api
+  -> AppT api t (EventWriterT t Loc m)
+  -> (Err -> EventWriterT t Loc m ())
+  -> m ()
+serve api ws showError = mdo
+  let ctx = undefined :: Context () t
+  dUrl <- url eUrl
+  (_, eUrl) <- runEventWriterT $ dyn $ either showError id . (route api ctx ws =<<) <$> dUrl
+  pure ()
+
+data Context ctx t
 
 -- data Context contextTypes where
 --     EmptyContext :: Context '[]
@@ -89,63 +137,6 @@ url us = do
 -- instance {-# OVERLAPPING #-} HasContextEntry (val ': xs) val where
 --   getContextEntry (x :. _) = x
 
-
-app :: MonadWidget t m => m ()
-app = serve (Proxy @(ToServantApi Routes)) (toServant widgets)
-
-widgets :: forall t m. MonadWidget t m => Routes (AsApp t (EventWriterT t Loc m))
-widgets = Routes
-  { showUserRoute = \(i :: Int) -> text (T.pack $ show i)
-  , homeRoute = do
-      e <- button "Go to user"
-      tellEvent $ Loc ["user", "55"] [] <$ e
-  }
-
-data Routes route = Routes
-  { showUserRoute :: route :- "user" :> Capture "id" Int :> Get '[App] ()
-  , homeRoute :: route :- Get '[App] ()
-  } deriving (Generic)
-
-data Context ctx t
-
--- Think about /error?redirect=/admin/x
-serve ::
-     forall t m api. (HasApp api (), MonadWidget t m)
-  => Proxy api
-  -> AppT api t (EventWriterT t Loc m)
-  -> m ()
-serve api widgets = mdo
-  let showError :: Show a => a -> EventWriterT t Loc m ()
-      showError = text . T.pack . show
-  dUrl <- url eUrl
-  let ctx = undefined :: Context () t
-  let app = either showError (either showError id . route api ctx widgets) <$> dUrl
-  (_, eUrl) <- runEventWriterT $ dyn app
-  pure ()
-
-data Loc = Loc
-  { locPath :: [ByteString]
-  , locQuery :: [(ByteString, ByteString)]
-  }
-instance Semigroup Loc where
-  a <> _ = a
-
-data Err
-  = Err400 Text
-  | Err401
-  | Err404
-  | Err501
-  deriving (Eq, Ord, Show)
-
-data App
-
-instance Accept App where
-  contentType _ = "text" // "html" /: ("charset", "utf-8")
-
--- Render a hydrated app on the server
--- instance MimeRender App a where
---   mimeRender _ _ = ...
-
 data AsApp (t :: *) (m :: * -> *)
 
 instance GenericMode (AsApp t m) where
@@ -155,12 +146,10 @@ class HasApp api ctx where
   type AppT api t (m :: * -> *) :: *
   route :: Proxy api -> context ctx t -> AppT api t m -> Loc -> Either Err (m ())
 
--- :<|>
 instance (HasApp a ctx, HasApp b ctx) => HasApp (a :<|> b) ctx where
   type AppT (a :<|> b) t m = AppT a t m :<|> AppT b t m
   route _ ctx (a :<|> b) = route (Proxy @a) ctx a <> route (Proxy @b) ctx b
 
--- Symbol :>
 instance (KnownSymbol sym, HasApp sub ctx) => HasApp (sym :> sub) ctx where
   type AppT (sym :> sub) t m = AppT sub t m
   route _ ctx f loc = case locPath loc of
@@ -170,7 +159,6 @@ instance (KnownSymbol sym, HasApp sub ctx) => HasApp (sym :> sub) ctx where
       then route (Proxy @sub) ctx f (loc { locPath = ps })
       else Left Err404
 
--- Capture sym a :>
 instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (Capture sym a :> sub) ctx where
   type AppT (Capture sym a :> sub) t m = a -> AppT sub t m
   route _ ctx f loc = case locPath loc of
@@ -182,7 +170,6 @@ instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (Capture sym a 
             v = T.pack $ BC.unpack x
         in Left $ Err400 ("Error parsing path part '" <> v <> "' ('" <> s <> "'")
 
--- QueryParam sym a :>
 instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (QueryParam sym a :> sub) ctx where
   type AppT (QueryParam sym a :> sub) t m = Maybe a -> AppT sub t m
   route _ ctx f loc = case find ((s ==) . fst) (locQuery loc) of
@@ -195,7 +182,6 @@ instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (QueryParam sym
     where
       s = BC.pack $ symbolVal (Proxy @sym)
 
--- QueryParams sym a :>
 instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (QueryParams sym a :> sub) ctx where
   type AppT (QueryParams sym a :> sub) t m = [a] -> AppT sub t m
   route _ ctx f loc = case partition ((s ==) . fst) (locQuery loc) of
@@ -208,16 +194,136 @@ instance (FromJSON a, KnownSymbol sym, HasApp sub ctx) => HasApp (QueryParams sy
     where
       s = BC.pack $ symbolVal (Proxy @sym)
 
+data App
+instance HasApp App ctx where
+  type AppT App t m = m ()
+  route _ _ f loc = case locPath loc of
+    [] -> Right f
+    [""] -> Right f
+    _ -> Left Err404
+
 -- AuthProtect tag :>
 -- data Authorized
 -- instance (HasContextElement AuthCheck ctx, HasApp sub ctx) => HasApp (AuthProtect Authorized :> sub) ctx where
 --   type AppT (AuthProtect Authorized :> sub) t m = AppT sub t m
 --   route _ ctx f = route (Proxy @sub) ctx f
 
--- Verb - IsElem App contents, method ~ Get?
-instance (Elem App ct) => HasApp (Verb 'GET st ct a) ctx where
-  type AppT (Verb 'GET st ct a) t m = m ()
-  route _ _ f loc = case locPath loc of
-    [] -> Right f
-    [""] -> Right f
-    _ -> Left Err404
+unconsT ::
+     forall a hd tl. (a ~ (hd : tl), TupleProduct tl)
+  => Proxy a
+  -> TupleProductOf a
+  -> (hd, TupleProductOf tl)
+unconsT _ a = (tupleHead (Proxy @tl) a, tupleTail (Proxy @tl) (Proxy @hd) a)
+
+class TupleProduct ts where
+  type TupleProductOf (ts :: [*]) :: *
+  tupleCons :: Proxy ts -> hd -> TupleProductOf ts -> TupleProductOf (hd ': ts)
+  tupleHead :: Proxy ts -> TupleProductOf (hd ': ts) -> hd
+  tupleTail :: Proxy ts -> Proxy hd -> TupleProductOf (hd ': ts) -> TupleProductOf ts
+
+instance TupleProduct '[] where
+  type TupleProductOf '[] = ()
+  tupleCons _ hd _ = hd
+  tupleHead _ hd = hd
+  tupleTail _ _ _ = ()
+
+instance TupleProduct '[a0] where
+  type TupleProductOf '[a0] = a0
+  tupleCons _ hd tl = (hd, tl)
+  tupleHead _ (a, _) = a
+  tupleTail _ _ (_, a) = a
+
+instance TupleProduct '[a1, a0] where
+  type TupleProductOf '[a1, a0] = (a1, a0)
+  tupleCons _ hd (a1, a0) = (hd, a1, a0)
+  tupleHead _ (a, _, _) = a
+  tupleTail _ _ (_, a1, a0) = (a1, a0)
+
+instance TupleProduct '[a2, a1, a0] where
+  type TupleProductOf '[a2, a1, a0] = (a2, a1, a0)
+  tupleCons _ hd (a2, a1, a0) = (hd, a2, a1, a0)
+  tupleHead _ (a, _, _, _) = a
+  tupleTail _ _ (_, a2, a1, a0) = (a2, a1, a0)
+
+instance TupleProduct '[a3, a2, a1, a0] where
+  type TupleProductOf '[a3, a2, a1, a0] = (a3, a2, a1, a0)
+  tupleCons _ hd (a3, a2, a1, a0) = (hd, a3, a2, a1, a0)
+  tupleHead _ (a, _, _, _, _) = a
+  tupleTail _ _ (_, a3, a2, a1, a0) = (a3, a2, a1, a0)
+
+instance TupleProduct '[a4, a3, a2, a1, a0] where
+  type TupleProductOf '[a4, a3, a2, a1, a0] = (a4, a3, a2, a1, a0)
+  tupleCons _ hd (a4, a3, a2, a1, a0) = (hd, a4, a3, a2, a1, a0)
+  tupleHead _ (a, _, _, _, _, _) = a
+  tupleTail _ _ (_, a4, a3, a2, a1, a0) = (a4, a3, a2, a1, a0)
+
+instance TupleProduct '[a5, a4, a3, a2, a1, a0] where
+  type TupleProductOf '[a5, a4, a3, a2, a1, a0] = (a5, a4, a3, a2, a1, a0)
+  tupleCons _ hd (a5, a4, a3, a2, a1, a0) = (hd, a5, a4, a3, a2, a1, a0)
+  tupleHead _ (a, _, _, _, _, _, _) = a
+  tupleTail _ _ (_, a5, a4, a3, a2, a1, a0) = (a5, a4, a3, a2, a1, a0)
+
+instance TupleProduct '[a6, a5, a4, a3, a2, a1, a0] where
+  type TupleProductOf '[a6, a5, a4, a3, a2, a1, a0] = (a6, a5, a4, a3, a2, a1, a0)
+  tupleCons _ hd tl = (hd, tl)
+  tupleHead _ (a, _) = a
+  tupleTail _ _ (_, tl) = tl
+
+instance TupleProduct (a7 ': a6 ': a5 ': a4 ': a3 ': a2 ': a1 ': a0 ': more) where
+  type TupleProductOf (a7 ': a6 ': a5 ': a4 ': a3 ': a2 ': a1 ': a0 ': more) =
+        (a7, TupleProductOf (a6 ': a5 ': a4 ': a3 ': a2 ': a1 ': a0 ': more))
+  tupleCons _ hd tl = (hd, tl)
+  tupleHead _ (a, _) = a
+  tupleTail _ _ (_, tl) = tl
+
+appLink ::
+     forall t m e rs.
+     ( EventWriter t Loc m
+     , Reflex t
+     , IsElem e (ToServantApi rs)
+     , GenericServant rs AsApi
+     , HasAppLink e
+     )
+  => (rs AsApi -> e)
+  -> Event t (TupleProductOf (GatherLinkArgs e))
+  -> m ()
+appLink _ args =
+  tellEvent $
+  safeAppLink (genericApi (Proxy :: Proxy rs)) (Proxy @e) (Loc [] []) <$> args
+
+safeAppLink ::
+     (IsElem e api, HasAppLink e)
+  => Proxy api
+  -> Proxy e
+  -> Loc
+  -> TupleProductOf (GatherLinkArgs e)
+  -> Loc
+safeAppLink _ = toAppLink
+
+class HasAppLink api where
+  type GatherLinkArgs api :: [*]
+  toAppLink :: Proxy api -> Loc -> TupleProductOf (GatherLinkArgs api) -> Loc
+
+-- All links
+-- instance (HasAppLink a cfg, HasAppLink b cfg) => HasAppLink (a :<|> b) cfg where
+--   type GatherLinkArgs (a :<|> b) = AppLink a cfg m :<|> AppLink b cfg m
+--   toAppLink _ p q r f = toAppLink (Proxy @a) p q r f :<|> toAppLink (Proxy @b) p q r f
+
+instance (KnownSymbol sym, HasAppLink sub) => HasAppLink (sym :> sub) where
+  type GatherLinkArgs (sym :> sub) = GatherLinkArgs sub
+  toAppLink _ l = toAppLink (Proxy @sub) $ l
+    { locPath = locPath l ++ [BC.pack . symbolVal $ Proxy @sym]
+    }
+
+instance ( ToJSON a
+         , HasAppLink sub
+         , TupleProduct (GatherLinkArgs sub)
+         ) =>
+         HasAppLink (Capture sym a :> sub) where
+  type GatherLinkArgs (Capture sym a :> sub) = a : GatherLinkArgs sub
+  toAppLink _ l (unconsT (Proxy @(a : GatherLinkArgs sub)) -> (r, rs)) =
+    toAppLink (Proxy @sub) (l {locPath = locPath l ++ [encode r]}) rs
+
+instance HasAppLink App where
+  type GatherLinkArgs App = '[]
+  toAppLink _ l _ = l
